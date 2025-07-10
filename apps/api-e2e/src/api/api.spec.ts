0f99e5ed34b82bb22e3608080797a76754108d98
Diego Lamarão <30639653+diegofer25@users.noreply.github.com>
2025-07-10 19:38:42 -0300

    Update e2e test for API info

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
 describe('GET /api', () => {
-  it('should return a message', async () => {
+  it('should return API info', async () => {
     const res = await axios.get(`/api`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toEqual(
+      expect.objectContaining({
+        service: 'Ekklesia API',
+        version: '1.0.0',
+      })
+    );
+    expect(typeof res.data.status).toBe('string');
   });
 });
